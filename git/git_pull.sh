8af7f37d82b4cd634b9bacc5ad910461a9b721cd
dennis <dennis.cpp@gmail.com>
2013-04-04 15:04:03 +0800

    update

--- git/git_pull.sh
+++ git/git_pull.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
-arr_folder=($(ls | awk '{print $0}'))
-arr_size=${#arr_folder[@]}
 
-for ((i=0;i<$arr_size;i++))
+#arr_folder=($(ls | awk '{print $0}'))
+#arr_size=${#arr_folder[@]}
+#
+#for ((i=0;i<$arr_size;i++))
+#do
+#{
+#	if [ -d ${arr_folder[${i}]} ]; then
+#		( cd ${arr_folder[${i}]}; echo update `pwd`; git pull )
+#	fi
+#}
+#done
+
+#for i in ${arr_folder[@]}
+#do
+#{
+#	if [ -d $i ]; then
+#		(cd $i; echo update `pwd`; git pull)
+#	fi
+#}
+#done
+
+for i in `ls`
 do
 {
-	if [ -d ${arr_folder[${i}]} ]; then
-		( cd ${arr_folder[${i}]}; echo update `pwd`; git pull )
+	if [ -d $i ]; then
+		(cd $i; echo update `pwd`; git pull)
 	fi
 }
 done
