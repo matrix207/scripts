a65065f6e2818a75bb71c0da69b3467e81e4bede
dennis_dell <dennis.cpp@gmail.com>
2013-04-23 22:33:44 +0800

    update

--- git/git_clone.sh
+++ git/git_clone.sh
@@ -6,10 +6,10 @@
 ################################################################################
 
 url=https://github.com/matrix207/
-repostories=(matrix207.github.com shell config unpv3 vim VC c ldd emacs.d euler note pyfunny)
+repostories=(matrix207.github.com shell config unpv3 vim VC C ldd emacs.d euler note pyfunny)
 repos_size=${#repostories[@]}
 
-for ((i=1;i<$repos_size;i++))
+for ((i=0;i<$repos_size;i++))
 do
     if [ ! -d "${repostories[${i}]}" ]; then
         git clone ${url}${repostories[${i}]}.git
