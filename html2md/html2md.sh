4bea033ab3ab28f860285b7d6fc4cd6fdaf34683
dennis <dennis.cpp@gmail.com>
2013-04-29 13:46:38 +0800

    convert html to markdown

---
+++ html2md/html2md.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#grep "<DT><H3" bookmarks.html
+#grep -E "<DT><H3|HREF" bookmarks.html
+#awk -F'HREF="|" ADD_DATE|">|</A>' '{print $4,$2}' bookmarks.html | grep http > blog.txt
+#
+#vim:
+#%s#<DT><H3.*">\(.*\)</H3>#^\1$#g
+#
+
