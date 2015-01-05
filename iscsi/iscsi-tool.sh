3132dbfb6f92dd9f4bdc89770891efb0a74d02e2
Dennis <dennis.cpp@gmail.com>
2015-01-05 11:52:40 +0800

    update

--- iscsi/iscsi-tool.sh
+++ iscsi/iscsi-tool.sh
@@ -5,7 +5,13 @@ VERSION=1.0
 BUILD_TIME="compiled 2015-01-04 19:10"
 COPY_RIGHT="Copyright (C) 2015. All rights reserved."
 
-function logout_all()
+function iscsi_discovery()
+{
+	local ip=$1
+	iscsiadm -m discovery -t st -p $ip
+}
+
+function iscsi_logout_all()
 {
 	lsscsi |wc -l
 	iscsiadm -m node -U all
@@ -13,11 +19,10 @@ function logout_all()
 	iscsiadm -m node
 }
 
-function login_all()
+function iscsi_login_all()
 {
-	ip=$1
-	iscsiadm -m discovery -t st -p $ip
-	iscsiadm -m discovery -t st -p $ip
+	local ip=$1
+	iscsi_discovery $ip
 	iscsiadm -m node -L all
 }
 
@@ -25,13 +30,15 @@ function login_all()
 # Usage
 ############################################################
 usage() {
-	echo "Usage: $PROG [-U ] [-L ip_addr] [-h] [-v]"
+	echo "Usage: $PROG [-D ip] [-U ] [-L ip] [-h] [-v]"
+	echo "            -D ip, discovery"
 	echo "            -U, logout all"
-	echo "            -L ipaddr, login all"
+	echo "            -L ip, login all"
 	echo "            -h, show help"
 	echo "            -v, show version"
 	echo " "
 	echo "EXAMPLES"
+	echo "      $PROG -D 172.16.130.100"
 	echo "      $PROG -U "
 	echo "      $PROG -L 172.16.130.100"
 	echo "      $PROG -h"
@@ -53,14 +60,18 @@ version() {
 # Handles input
 ############################################################
 input_getopts() {
-    while getopts ":L:Uv" opt; do
+    while getopts ":D:L:Uv" opt; do
 		case $opt in
+			D)
+				local ip=$OPTARG
+				iscsi_discovery $ip
+				;;
 			L)
-				local ip=$OTPARG
-				login_all $ip
+				local ip=$OPTARG
+				iscsi_login_all $ip
 				;;
 			U)
-				logout_all
+				iscsi_logout_all
 				;;
 			v)
 				version
